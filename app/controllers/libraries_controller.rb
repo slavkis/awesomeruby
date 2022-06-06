522ea25654f9227bae94deb50b0a02b9fd7693b9
slavkis <slavko.okunev@gmail.com>
2022-06-06 18:56:27 +0300

    Preview finished

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -1,10 +1,10 @@
 class LibrariesController < ApplicationController
   def index
     @libraries = Library.all
-    @categories = Category.all
+    # @categories = Category.all
 
     if params[:min_stars]
-      @libraries = @libraries.where(stars: (params[:min_stars]..@libraries.max(:stars)))
+      @libraries = @libraries.where(stars: (params[:min_stars].to_i..@libraries.maximum(:stars)))
     end
 
     @libraries = @libraries.group_by{ |lib| lib.category }
